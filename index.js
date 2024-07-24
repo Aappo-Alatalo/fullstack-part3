9ce2e99bc6eff66eb4811d5ef311874253630171
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-07-24 10:44:44 +0300

    Add exercises 3.2-3.6

--- index.js
+++ index.js
@@ -26,8 +26,63 @@ let persons = [
     }
 ]
 
+app.get('/info', (request, response) => {
+    response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>`)
+})
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+        return response.status(400).json({ 
+          error: 'name missing' 
+        })
+    } else if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+    }
+
+    const existingPerson = persons.find(person => person.name === body.name)
+    if (existingPerson) {
+      return response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: String(Math.floor(Math.random() * (5000 - 1 + 1)) + 1)
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
