be4c544ce4065805e82150f16be4fd2c45571def
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-08-06 21:12:29 +0300

    Add fully working version of exercise 3.21 deployment
    Before update didn't give validation errors due to frontend mistake.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
 app.use(cors())
@@ -18,60 +20,49 @@ const format = ':method :url :status :response-time ms :body'
 
 app.use(morgan(format))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
-    response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${Date()}</p>`)
+    Person.find({})
+        .then(people => {
+            const peopleCount = people.length
+            const date = new Date()
+            response.send(
+            `<p>Phonebook has info for ${peopleCount} people</p>
+            <p>${date}</p>`)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-  
+    
     if (!body.name) {
         return response.status(400).json({ 
           error: 'name missing' 
@@ -82,30 +73,63 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const existingPerson = persons.find(person => person.name === body.name)
-    if (existingPerson) {
-      return response.status(400).json({ 
-        error: 'name must be unique' 
-      })
-    }
+    // We don't need to care about double names per 3.14 instructions
+    // const existingPerson = persons.find(person => person.name === body.name)
+    // if (existingPerson) {
+    //   return response.status(400).json({ 
+    //     error: 'name must be unique' 
+    //   })
+    // }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: String(Math.floor(Math.random() * (5000 - 1 + 1)) + 1)
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save()
+        .then(result => {
+            console.log(`added ${result.name} number ${result.number} to phonebook`)
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
 
-const unknownEndpoint = (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response, next) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message }) 
+    } else {
+        console.log(error.name)
+    }
+    next(error)
+}
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
